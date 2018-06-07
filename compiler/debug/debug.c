5d0fb26e91dbaa65a3309492e06c04e7026d9a3c
Malik <hiraev@users.noreply.github.com>
2018-06-07 18:39:33 +0300

    [+MACROS] for switch constructor

--- compiler/debug/debug.c
+++ compiler/debug/debug.c
@@ -3,36 +3,22 @@
 //
 #include "debug.h"
 
+#define PRINT(id, ids) case id: printf("%-5s", ids); break;
+
 void print(struct Token *tokens) {
     int d = 0;
     while (tokens[d].type != TEND) {
         struct Token t = tokens[d];
         printf("%d\t%d\t", d, t.line);
         switch (t.type) {
-            case ID:
-                printf("ID   ");
-                break;
-            case KWORD:
-                printf("KWORD");
-                break;
-            case LBRC:
-                printf("LBRC ");
-                break;
-            case RBRC:
-                printf("RBRC ");
-                break;
-            case BINOP:
-                printf("BINOP");
-                break;
-            case NUM:
-                printf("NUM  ");
-                break;
-            case SCLN:
-                printf("SCLN ");
-                break;
-            case STR:
-                printf("STR  ");
-                break;
+            PRINT(ID, "ID")
+            PRINT(KWORD, "KWORD")
+            PRINT(LBRC, "LBRC")
+            PRINT(RBRC, "RBRC")
+            PRINT(BINOP, "BINOP")
+            PRINT(NUM, "NUM")
+            PRINT(SCLN, "SCLN")
+            PRINT(STR, "STR")
         }
         printf("\t%s\n", tokens[d].str);
         d++;
