def03e46c41682438140b347d2fc494fe92b1ca0
Malik <hiraev@users.noreply.github.com>
2018-09-23 21:50:15 +0300

    New debug function

--- compiler/debug/debug.c
+++ compiler/debug/debug.c
@@ -25,3 +25,69 @@ void print(struct Token *tokens) {
         d++;
     }
 }
+
+void print_2(struct ForGenerator *forGenerator) {
+
+    struct Instr* first_instr = forGenerator->instructions;
+    unsigned num_of_strings = forGenerator->num_of_strings;
+    char **strings = forGenerator->strings;
+
+    printf("\n\nALL STRINGS:\n");
+    for (int i = 0; i < num_of_strings; i++) {
+        printf("%s\n", strings[i]);
+    }
+
+    for (int i = 0; i < forGenerator->num_of_instructions; ++i) {
+        printf("%d ", i + 1);
+        switch (first_instr[i].type) {
+            case PRINT_INT:
+                printf("PRINT_INT: %d\n", first_instr[i].id);
+                break;
+            case PRINT_IMM:
+                printf("PRINT_IMM: %d\n", first_instr[i].expr->number);
+                break;
+            case PRINT_STR:
+                printf("PRINT_STR: %s\n", strings[first_instr[i].id]);
+                break;
+            case WRITE_INT:
+                printf("WRITE_INT: %d ", first_instr[i].id);
+                struct Expr *expr = first_instr[i].expr;
+                while (expr->type != E_END) {
+                    enum expr_elem_type expr_elem_type1 = expr->type;
+                    switch (expr_elem_type1) {
+                        case E_OPERATOR:
+                            switch (expr->oper) {
+                                case ADD:
+                                    printf(" ADD ");
+                                    break;
+                                case SUB:
+                                    printf(" SUB ");
+                                    break;
+                                case MUL:
+                                    printf(" MUL ");
+                                    break;
+                                case DIV:
+                                    printf(" DIV ");
+                                    break;
+                                case MOD:
+                                    printf(" MOD ");
+                                    break;
+                            }
+                            break;
+                        case E_NUMBER:
+                            printf(" %d ", expr->number);
+                            break;
+                        case E_ID:
+                            printf(" (id: %d) ", expr->id);
+                            break;
+                        case E_READ:
+                            printf(" read ");
+                            break;
+                    }
+                    expr++;
+                }
+                printf("\n");
+                break;
+        }
+    }
+}
--- compiler/debug/debug.h
+++ compiler/debug/debug.h
@@ -5,5 +5,7 @@
 #ifndef COMPILER_DEBUG_H
 #define COMPILER_DEBUG_H
 #include "../headers/lexer.h"
+#include "../headers/instruction.h"
 void print(struct Token * tokens);
+void print_2(struct ForGenerator *forGenerator);
 #endif //COMPILER_DEBUG_H
