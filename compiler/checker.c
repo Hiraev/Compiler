a452ebebd54bd6d60a8912079efa7b3d085f760a
Malik <hiraev@users.noreply.github.com>
2018-09-23 20:05:08 +0300

    A file for the semantic analyze

---
+++ compiler/checker.c
@@ -0,0 +1,12 @@
+//
+// Created by Malik Hiraev on 23.09.2018.
+//
+#include "headers/cheker.h"
+
+unsigned get_index(struct ID_map *id_map, char *id) {
+    while (true) {
+        if (!strcmp(id_map->id, id))
+            return id_map->index;
+        id_map++;
+    }
+}
