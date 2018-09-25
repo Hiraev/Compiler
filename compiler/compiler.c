7630e29b6b2110cc27917167bdfd604614ea493f
Malik <hiraev@users.noreply.github.com>
2018-09-25 14:36:14 +0300

    Finally, it can work with several files It requires now an extension - .mh

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -8,30 +8,43 @@
 #include "headers/semalyzer.h"
 #include "headers/code_generator.h"
 
-#define DEBUG
+//#define DEBUG
 
 int main(int num, char **c) {
     //  Слишком мало аргументов
     if (num < 2) exit_with_msg(ERR(NO_FILE), "Необходимо ввести название файла", 0);
 
-    FILE *file = fopen(c[1], "r");
+    for (int j = 1; j < num; ++j) {
+        FILE *file = fopen(c[j], "r");
+        //  Не нашли файл
+        if (file == NULL) exit_with_msg(ERR(CANT_OPEN), c[j], 0);
 
-    //  Не нашли файл
-    if (file == NULL) exit_with_msg(ERR(CANT_OPEN), c[1], 0);
+        //Проверяем расширение файла
+        unsigned i = 0;
+        do {
+            i++;
+        } while (c[j][i] != '\0');
+        //Если i <= 3 значит название файла слишком короткое, чтобы сожержать расширение
+        if (i <= 3 || c[j][i - 1] != 'h' || c[j][i - 2] != 'm' || c[j][i - 3] != '.') {
+            exit_with_msg(ERR(WRONG_EXPANSION), c[j], 0);
+        }
 
-    struct Token *tokens = lexer(file);
+        struct Token *tokens = lexer(file);
 #ifdef DEBUG
-    print(tokens);
+        print(tokens);
 #endif
-    struct Line *lines = synalyze(tokens);
+        struct Line *lines = synalyze(tokens);
 
-    //ПОЛУЧАЕМ ВАЛИДНЫЙ КОД, ЕСЛИ ФУНКЦИЯ ОТРАБАТЫВАЕТ УСПЕШНО
-    //Такие же Line, что и в предыдущем, только выражения в обратной польской нотации
-    struct ForGenerator *forGenerator = semanalyze(lines);
+        struct ForGenerator *forGenerator = semanalyze(lines);
 #ifdef DEBUG
-    print_2(forGenerator);
+        print_2(forGenerator);
 #endif
-    FILE *out = fopen("a.s", "w");
-    generate(out, forGenerator);
+        char *out = c[j];
+        out[i - 2] = 's';
+        out[i - 1] = '\0';
+        FILE *out_file = fopen(out, "w");
+        generate(out_file, forGenerator);
+        printf("Файл: %s успешно скомпилирован\n", c[j]);
+    }
     return 0;
 }
